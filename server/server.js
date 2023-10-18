01e16e87b94700bf4587ad813de3f3a63f887a7d
Oleksandr Zavialov <oleksandr_zavialov@epam.com>
2023-10-18 17:45:52 +0300

    feat(add

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://dslifeim:223132wwQ!@cluster0.voruyv2.mongodb.net/auth?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
